e429cf48de418b6ed6e4c1e04b898283d85e910f
yanglindong0725 <1009444100@qq.com>
2024-06-26 14:12:52 +0800

    feat: 添加背景

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-    <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
-    <div class="max-w-3xl mx-auto">
-      <header class="flex flex-col items-center justify-center h-screen">
-        <div class="text-center">
-          <h1 class="text-4xl font-bold tracking-tight sm:text-5xl text-slate-800">
-            开箱即用的<span
-              class="text-transparent bg-clip-text bg-gradient-to-br from-green-400 from-10% to-sky-500 to-70% w-fit px-1"
-              >Vue3</span
-            >项目
-          </h1>
-          <p class="mt-6 text-lg leading-8 text-gray-600">
-            本项目基于 Vue3 + Vite2 + TailwindCSS + Pinia +
-            Naive，内置了路由、状态管理、国际化、主题切换、错误处理，让你专注于业务开发，无需再花费时间在搭建项目上。
-          </p>
-        </div>
-      </header>
+  <div class="bg-gradient-to-br to-rose-400/30 from-10% from-sky-500/60 to-90%">
+    <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
+      <div class="max-w-3xl mx-auto">
+        <header class="flex flex-col items-center justify-center h-screen">
+          <div class="text-center">
+            <h1 class="text-4xl font-bold tracking-tight sm:text-5xl text-slate-800">
+              开箱即用的<span
+                class="text-transparent bg-clip-text bg-gradient-to-br from-green-400 from-10% to-sky-500 to-70% w-fit px-1"
+                >Vue3</span
+              >项目
+            </h1>
+            <p class="mt-6 text-lg leading-8 text-gray-600">
+              本项目基于 Vue3 + Vite2 + TailwindCSS + Pinia +
+              Naive，内置了路由、状态管理、国际化、主题切换、错误处理，让你专注于业务开发，无需再花费时间在搭建项目上。
+            </p>
+          </div>
+        </header>
+      </div>
     </div>
   </div>
 </template>
